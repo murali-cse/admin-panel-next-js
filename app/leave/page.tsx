116cf6fb3e4f93c205f79d934ea14da90a50086c
murali-cse <muralicse97@gmail.com>
2023-11-15 23:28:04 +0530

    Coming soon page added on leave page

--- app/leave/page.tsx
+++ app/leave/page.tsx
@@ -4,8 +4,13 @@ import PageTitle from "../components/PageTitle";
 const Leave = () => {
   return (
     <main>
-      <div className="p-4">
-        <PageTitle>Leave</PageTitle>
+      <div className="p-4 h-screen">
+        <div>
+          <PageTitle>Leave</PageTitle>
+        </div>
+        <div className="flex justify-center items-center h-5/6">
+          <p>Coming Soon...</p>
+        </div>
       </div>
     </main>
   );
