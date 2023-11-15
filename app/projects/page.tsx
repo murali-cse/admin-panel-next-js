a0ac8d27cde97954f3c96d0efc026c2c1368a1e0
murali-cse <muralicse97@gmail.com>
2023-11-15 23:28:33 +0530

    no project found page added

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import PageTitle from "../components/PageTitle";
-import { Button } from "flowbite-react";
 import AddButton from "./AddButton";
 
 const Projects = () => {
   return (
     <main>
-      <div className="p-4 flex justify-between items-center">
-        <PageTitle>Projects</PageTitle>
-        <AddButton />
+      <div className="p-4 h-screen">
+        <div className="flex justify-between items-center">
+          <PageTitle>Projects</PageTitle>
+          <AddButton />
+        </div>
+        <div className="flex justify-center items-center h-5/6">
+          <p>No Projects Found</p>
+        </div>
       </div>
-      <div className=""></div>
     </main>
   );
 };
