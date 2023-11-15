ad54262b450280b193ec3481bd41eb1deeffd8dd
murali-cse <muralicse97@gmail.com>
2023-11-15 23:39:31 +0530

    loading pages for projects

--- app/holidays/page.tsx
+++ app/holidays/page.tsx
@@ -5,9 +5,12 @@ import PageTitle from "../components/PageTitle";
 const Holidays = () => {
   return (
     <main>
-      <div className="p-4">
+      <div className="p-4 h-screen">
         <div className="flex justify-between items-center">
           <PageTitle>Holidays</PageTitle>
+        </div>
+        <div className="h-5/6 flex justify-center items-center">
+          <p>Coming Soon...</p>
         </div>
       </div>
     </main>
--- app/projects/loading.tsx
+++ app/projects/loading.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import PageTitle from "../components/PageTitle";
 
 const ProjectsLoading = () => {
-  return <div>Projects Loading ...</div>;
+  return (
+    <main>
+      <div className="p-4 h-screen">
+        <div className="flex justify-between items-center">
+          <PageTitle>Projects</PageTitle>
+        </div>
+        <div className="flex justify-center items-center h-5/6">
+          <p>Loading ...</p>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default ProjectsLoading;
